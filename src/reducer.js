322bd00e0a5e92e5a0799b31fbfad960a357f434
Tamara Bazko <tamarabazko@gmail.com>
2018-03-18 19:19:53 +0100

    Remove counter (example module)

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
 import checkListReducer from 'modules/checkList/reducer'
-import counterReducer from 'modules/counter/reducer'
 
 const rootReducer = combineReducers({
-  counter: counterReducer,
   checkList: checkListReducer,
 })
 
